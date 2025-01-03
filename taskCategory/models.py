f17a109d7a8834fabfea72c3bdc78bf4d60281bb
DESKTOP-9MEJLQT\BOGURA <s.m.tariqul.islam.eu181400082@gmail.com>
2025-01-03 19:16:15 +0600

    complete assignment need to check

--- taskCategory/models.py
+++ taskCategory/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from taskModel.models import TaskModel
 
 # Create your models here.
 
 class TaskCategory(models.Model):
     name = models.CharField(max_length=100)
-    taskModel = models.ManyToManyField(TaskModel)
 
     def __str__(self):
         return self.name
--- taskModel/models.py
+++ taskModel/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+from  taskCategory.models import TaskCategory
+
+
 class TaskModel(models.Model):
     title=models.CharField(max_length=100)
     decription=models.TextField()
     assignDate=models.DateField()
+    category = models.ManyToManyField(TaskCategory)
     is_completed =models.BooleanField(default=False)
 
     def __str__(self):
